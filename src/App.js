ca54d84ceec97b8e7ddb45083260ca5971f1bdd8
jmill29 <jacoblmiller.jlm@gmail.com>
2024-01-16 12:53:10 -0500

    Adds (mostly) completed application. Note: All features are fully operational with the exception of the playlist-saving capability, which is currently under progress.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import SearchResults from './SearchResults';
+import Playlist from './Playlist';
+import SearchBar from './SearchBar';
+import LoginPage from './LoginPage';
+import searchForSongs from './api_calls/searchForSongs';
 
 function App() {
+  document.body.style.backgroundColor = 'deepskyblue';
+
+  
+  const [songs, setSongs] = useState([]);
+  const [plSongUris, setPlSongUris] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResultSongs, setSearchResultSongs] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [playlistName, setPlaylistName] = useState('');
+  const [userInfo, setUserInfo] = useState({api_key: '', userId: ''});
+
+  const url = 'https://api.spotify.com/v1';
+
+  const handleClick = () => {
+    setLoggedIn(false);
+    setSongs([]);
+    setPlSongUris([]);
+    setSearch('');
+    setSearchResultSongs([]);
+    setPlaylistName('');
+  }
+
+  useEffect(() => {
+    if (search) {
+      let params = {
+        api_key: userInfo.api_key,
+        searchQuery: search,
+        setSearchResultSongs: setSearchResultSongs
+      };
+      searchForSongs(params);
+    }
+  }, [search])
+
+  useEffect(() => {
+    if (userInfo.api_key && userInfo.userId) {
+      console.log(userInfo);
+    }
+  }, [userInfo]);
+
+  /*useEffect(() => {
+    console.log(playlistName);
+  }, [playlistName]);*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loggedIn ? (
+        <>
+          <SearchBar setSearch={setSearch} handleClick={handleClick} />
+          <div style={{display: 'flex', justifyContent: 'center', marginTop: 20}}>
+            <SearchResults setSongs={setSongs} data={searchResultSongs} plSongUris={plSongUris} setPlSongUris={setPlSongUris} />
+            <Playlist playlistSongs={songs} setSongs={setSongs} plSongUris={plSongUris} setPlSongUris={setPlSongUris} setPlaylistName={setPlaylistName} />
+          </div>
+        </>): (
+        <>
+          <LoginPage setLoggedIn={setLoggedIn} setUserInfo={setUserInfo} />
+        </>
+      )}
+    </>
   );
 }
 
